1102b3558af86d30379516ca3b531d463ddf5324
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-08 12:47:46 +0200

    Added some fish aliases ll,la

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -11,7 +11,8 @@ fzf_configure_bindings --directory=\cf
 set fzf_preview_dir_cmd ls --all --color=always --tree=2
 
 alias ls "colorls --sd"
-alias la "colorls -lAog --time-style=\"+%Y/%m/%d %H:%M:%S\" --sd"
+alias ll "ls -log --time-style=\"+%Y/%m/%d %H:%M:%S\" --sd"
+alias la "ls -lAog --time-style=\"+%Y/%m/%d %H:%M:%S\" --sd"
 
 alias fishconf "nvim ~/.config/fish/config.fish"
 alias dotconf "cd ~/repos/dotfiles"
