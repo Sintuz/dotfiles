c162a94ff67080e438dda7785d007dc9b2d1ac7b
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-09 16:20:24 +0200

    Some nvim remaps to move between windows with just <C-[hjkl]>

--- nvim/dot-config/nvim/lua/config/remap.lua
+++ nvim/dot-config/nvim/lua/config/remap.lua
@@ -22,3 +22,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
