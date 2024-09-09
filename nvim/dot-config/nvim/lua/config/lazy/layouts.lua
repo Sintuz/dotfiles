5500abc0d2655939976233d650b96c3fc3a8514c
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-09 22:33:38 +0200

    Disabled nvim barbar tab manager

--- nvim/dot-config/nvim/lua/config/lazy/layouts.lua
+++ nvim/dot-config/nvim/lua/config/lazy/layouts.lua
@@ -1,5 +1,5 @@
 return {
-    {
+   --[[ {
         'romgrk/barbar.nvim',
         dependencies = {
             'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
@@ -7,13 +7,12 @@ return {
         },
         init = function() vim.g.barbar_auto_setup = false end,
         opts = {
-            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-            -- animation = true,
+            animation = true,
             -- insert_at_start = true,
             -- …etc.
         },
         version = '^1.0.0', -- optional: only update when a new 1.x version is released
-    },
+    },--]]
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
