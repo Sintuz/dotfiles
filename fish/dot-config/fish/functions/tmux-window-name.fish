8c1027b23fe6bd551bfd5c6cbd1034b43231e57e
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-11 00:43:22 +0200

    Update tmux window name when opening new fish shell

--- fish/dot-config/fish/functions/tmux-window-name.fish
+++ fish/dot-config/fish/functions/tmux-window-name.fish
@@ -1,3 +1,6 @@
 function tmux-window-name --on-variable PWD
     ~/.tmux/plugins/tmux-window-name/scripts/rename_session_windows.py &> /dev/null 2>&1
 end
+if test -n "$TMUX"
+    tmux-window-name
+end
