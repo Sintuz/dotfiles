2da248763e81b0a2d5dd02982f1da7da552ff2f9
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-11 20:04:19 +0200

    Move tmux plugin folder

--- fish/dot-config/fish/functions/tmux-window-name.fish
+++ fish/dot-config/fish/functions/tmux-window-name.fish
@@ -1,5 +1,5 @@
 function tmux-window-name --on-variable PWD
-    ~/.tmux/plugins/tmux-window-name/scripts/rename_session_windows.py &> /dev/null 2>&1
+    $TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &> /dev/null 2>&1
 end
 if test -n "$TMUX"
     tmux-window-name
--- nvim/dot-config/nvim/lua/config/init.lua
+++ nvim/dot-config/nvim/lua/config/init.lua
@@ -5,7 +5,7 @@ require('config.lazy_init')
 vim.api.nvim_create_autocmd({ 'VimEnter', 'VimLeave' }, {
     callback = function()
         if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-            local pid = vim.fn.jobstart('~/.tmux/plugins/tmux-window-name/scripts/rename_session_windows.py &')
+            local pid = vim.fn.jobstart(vim.env.TMUX_PLUGIN_MANAGER_PATH .. '/tmux-window-name/scripts/rename_session_windows.py &')
             vim.fn.jobwait({ pid })
         end
     end,
