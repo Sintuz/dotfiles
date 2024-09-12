8c8ebaa7a28a192c09f3dcca455ff73b56b261b0
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-12 15:14:14 +0200

    Update fish theme and enable vi mode

---
+++ fish/dot-config/fish/conf.d/bobthefish.fish
@@ -0,0 +1,15 @@
+set -g theme_display_vi yes
+set -g theme_show_exit_status yes
+
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+
+set -g theme_display_git_default_branch yes
+
+set -g theme_date_format "+%Y-%m-%d %H:%M:%S"
+
+set -g theme_newline_cursor yes
+
+set -g theme_color_scheme catppuccin-mocha
+set -g theme_nerd_fonts yes
+
