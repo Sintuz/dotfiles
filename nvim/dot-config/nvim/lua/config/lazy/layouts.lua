0d78c152fb5b32626179a559e2034b2115c75750
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-12 11:12:57 +0200

    Fixes nvim status line issues with traspacency of the background

--- nvim/dot-config/nvim/lua/config/lazy/layouts.lua
+++ nvim/dot-config/nvim/lua/config/lazy/layouts.lua
@@ -18,7 +18,11 @@ return {
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
             require('lualine').setup({
-                options = { theme = 'catppuccin' },
+                options = {
+                    theme = 'catppuccin',
+                    component_separators = { left = '', right = '' },
+                    section_separators = { left = '', right = '' },
+                },
                 sections = {
                     lualine_a = { 'mode' },
                     lualine_b = { 'branch', 'diff', 'diagnostics' },
