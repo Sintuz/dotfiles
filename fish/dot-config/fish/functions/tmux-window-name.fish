0076ee7fe9961dc090ccc596eb851f70c9709f08
Enrico Sintoni <enrico.sintoni.ra@gmail.com>
2024-09-12 11:27:53 +0200

    Fix fish call update tmux only when inside a session

--- fish/dot-config/fish/functions/tmux-window-name.fish
+++ fish/dot-config/fish/functions/tmux-window-name.fish
@@ -1,5 +1,7 @@
 function tmux-window-name --on-variable PWD
-    $TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &> /dev/null 2>&1
+    if test -n "$TMUX"
+        $TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &> /dev/null 2>&1
+    end
 end
 if test -n "$TMUX"
     tmux-window-name
